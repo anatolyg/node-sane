394c1639f6536e73c7ef62da6106ca5ef89a7df5
Anatoly Geyfman <anatoly@geyfman.net>
2011-09-24 23:57:26 -0700

    fixed uuid gen

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.configure('production', function(){
 });
 
 function uuid(a){
-    return a?(0|Math.random()*16).toString(16):(""+1e7+-1e3+-4e3+-8e3+-1e11).replace(/1|0/g,b)
+    return a?(0|Math.random()*16).toString(16):(""+1e7+-1e3+-4e3+-8e3+-1e11).replace(/1|0/g,uuid)
 }
 
 
